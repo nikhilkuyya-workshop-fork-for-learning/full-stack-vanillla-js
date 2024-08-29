e9e8407be1dbea6a010441f18bb1e5222bbaba08
nikhilkuyya <nikhilkuyya@gmail.com>
2024-08-29 18:49:10 +0530

    feat: clean up test cases

--- test/web.test.js
+++ test/web.test.js
@@ -1,8 +1,101 @@
-import { describe, it } from 'node:test';
+import { describe, it, mock } from 'node:test';
+import Controller from './../src/shared/controller.js';
+import WebView from './../src/platforms/web/view.js';
+import assert from 'node:assert';
 
+globalThis.alert = mock.fn();
+function setglobalDocument(mock, mockData) {
+  globalThis.document = {
+    createElement: mock.fn(() => {
+      return {
+        classList: {
+          add: mock.fn((newClass) => {}),
+        }
+      }
+    }),
+    querySelector: mock.fn((selector) => {
+      const mockValue = mockData[selector.replace('#','')] || '';
+      return {
+        value: mockValue,
+        reset: mock.fn(),
+        appendChild: mock.fn((element) => {}),
+        addEventListener: mock.fn((event,fn) => {})
+      }
+    }),
+  };
+}
 
-describe('web ', () => {
-  it('working',() => {
-    0;
+describe('web app test suite ', () => {
+
+  it('submit with proper should add to table', () => {
+    setglobalDocument(mock,{name: 'test', age: 2, email: 'email@mail.com'});
+    const view = new WebView();
+    const addRow = mock.method(view, view.addRow.name);
+    const resetForm = mock.method(view,view.resetForm.name);
+
+    Controller.init({
+      view: view
+    })
+
+    const [
+      name,
+      age,
+      email,
+      table,
+      form,
+      btnClear
+    ] = document.querySelector.mock.calls;
+
+    assert.strictEqual(name.arguments[0],'#name');
+    assert.strictEqual(age.arguments[0],'#age');
+    assert.strictEqual(email.arguments[0],'#email');
+    assert.strictEqual(table.arguments[0],'.flex-table');
+    assert.strictEqual(form.arguments[0],'#form');
+    assert.strictEqual(btnClear.arguments[0],'#btnFormClear');
+
+    assert.strictEqual(addRow.mock.callCount(),3);
+
+    const submitCallBack = form.result.addEventListener.mock.calls[0].arguments[1];
+    const preventDefaultSpy = mock.fn();
+    submitCallBack({
+      preventDefault: preventDefaultSpy
+    });
+
+    assert.strictEqual(addRow.mock.callCount(),4);
+    assert.deepStrictEqual(addRow.mock.calls.at(3).arguments[0],
+    {name: 'test', age : 2, email: 'email@mail.com'});
+
+    assert.strictEqual(resetForm.mock.callCount(),1);
   })
+
+  it('should call notify when submitted with no proper value', () => {
+      setglobalDocument(mock,{name: '', age: 2, email: 'email@mail.com'});
+      const view = new WebView();
+      const addRow = mock.method(view, view.addRow.name);
+      const resetForm = mock.method(view,view.resetForm.name);
+      const notify = mock.method(view,view.notify.name);
+
+      Controller.init({
+        view: view
+      })
+
+      const [
+        name,
+        age,
+        email,
+        table,
+        form,
+        btnClear
+      ] = document.querySelector.mock.calls;
+
+      const submitCallBack = form.result.addEventListener.mock.calls[0].arguments[1];
+      const preventDefaultSpy = mock.fn();
+      assert.strictEqual(addRow.mock.callCount(),3);
+      submitCallBack({
+        preventDefault: preventDefaultSpy
+      });
+      assert.strictEqual(addRow.mock.callCount(),3);
+      assert.strictEqual(resetForm.mock.callCount(),0);
+      assert.strictEqual(notify.mock.callCount(),1);
+    });
 })
