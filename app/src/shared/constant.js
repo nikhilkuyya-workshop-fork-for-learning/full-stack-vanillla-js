3d1d5bb453a25cc4859dc756b872c2d793ff0051
nikhilkuyya <nikhilkuyya@gmail.com>
2024-08-30 21:32:00 +0530

    feat: setup different url

--- app/src/shared/constant.js
+++ app/src/shared/constant.js
@@ -1,1 +1,7 @@
-export const API_URL = `http://localhost:3000`
+let API_URL;
+if(process.env.ENV === 'prod'){
+  API_URL = 'https://full-stack-vanillla-js.onrender.com/'
+}else {
+  API_URL = `http://localhost:3000`
+}
+export {API_URL};
