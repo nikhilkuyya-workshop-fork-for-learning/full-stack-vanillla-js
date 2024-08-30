bb0dc95f62a9b105c479951e86a5b7b8955ab5f7
nikhilkuyya <nikhilkuyya@gmail.com>
2024-08-30 21:40:19 +0530

    fix: path

--- app/src/shared/constant.js
+++ app/src/shared/constant.js
@@ -1,6 +1,6 @@
 let API_URL;
 if(window.location.origin === 'https://nikhilkuyya-workshop-fork-for-learning.github.io'){
-  API_URL = 'https://full-stack-vanillla-js.onrender.com/'
+  API_URL = 'https://full-stack-vanillla-js.onrender.com'
 }else {
   API_URL = `http://localhost:3000`
 }
